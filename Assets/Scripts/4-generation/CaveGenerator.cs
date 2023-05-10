37d49f45148302501f7d84b81413823a89074215
tommyg141 <tommymordhi@gmail.com>
2023-05-10 11:42:20 +0300

    whitespace

--- Assets/Scripts/4-generation/CaveGenerator.cs
+++ Assets/Scripts/4-generation/CaveGenerator.cs
@@ -5,15 +5,16 @@ using System;
  * The map is generated as a two-dimensional array of ints, where "0" denotes floor and "1" denotes wall.
  * Initially, the boundaries of the cave are set to "wall", and the inner cells are set at random.
  * Then, a cellular automaton is run in order to smooth out the cave.
- * 
- * Based on Unity tutorial https://www.youtube.com/watch?v=v7yyZZjF1z4 
+ *
+ * Based on Unity tutorial https://www.youtube.com/watch?v=v7yyZZjF1z4
  * Code by Habrador: https://github.com/Habrador/Unity-Programming-Patterns/blob/master/Assets/Patterns/7.%20Double%20Buffer/Cave/GameController.cs
  * Using a double-buffer technique explained here: https://github.com/Habrador/Unity-Programming-Patterns#7-double-buffer
- * 
+ *
  * Adapted by: Erel Segal-Halevi
  * Since: 2020-12
  */
-public class CaveGenerator {
+public class CaveGenerator
+{
     //Used to init the cellular automata by spreading random dots on a grid,
     //and from these dots we will generate caves.
     //The higher the fill percentage, the smaller the caves.
@@ -26,10 +27,10 @@ public class CaveGenerator {
     private int[,] bufferOld;
     private int[,] bufferNew;
 
-
     private Random random;
 
-    public CaveGenerator(float randomFillPercent=0.5f, int gridSize=100) {
+    public CaveGenerator(float randomFillPercent = 0.5f, int gridSize = 100)
+    {
         this.randomFillPercent = randomFillPercent;
         this.gridSize = gridSize;
 
@@ -39,24 +40,29 @@ public class CaveGenerator {
         random = new Random();
     }
 
-    public int[,] GetMap() {
+    public int[,] GetMap()
+    {
         return bufferOld;
     }
-
-
 
     /**
      * Generate a random map.
      * The map is not smoothed; call Smooth several times in order to smooth it.
      */
-    public void RandomizeMap()  {
+    public void RandomizeMap()
+    {
         //Init the old values so we can calculate the new values
-        for (int x = 0; x < gridSize; x++) {
-            for (int y = 0; y < gridSize; y++) {
-                if (x == 0 || x == gridSize - 1 || y == 0 || y == gridSize - 1) {
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                if (x == 0 || x == gridSize - 1 || y == 0 || y == gridSize - 1)
+                {
                     //We dont want holes in our walls, so the border is always a wall
                     bufferOld[x, y] = 1;
-                } else {
+                }
+                else
+                {
                     //Random walls and caves
                     bufferOld[x, y] = random.NextDouble() < randomFillPercent ? 1 : 0;
                 }
@@ -64,16 +70,19 @@ public class CaveGenerator {
         }
     }
 
-
     /**
      * Generate caves by smoothing the data
      * Remember to always put the new results in bufferNew and use bufferOld to do the calculations
      */
-    public void SmoothMap()   {
-        for (int x = 0; x < gridSize; x++) {
-            for (int y = 0; y < gridSize; y++) {
+    public void SmoothMap()
+    {
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
                 //Border is always wall
-                if (x == 0 || x == gridSize - 1 || y == 0 || y == gridSize - 1) {
+                if (x == 0 || x == gridSize - 1 || y == 0 || y == gridSize - 1)
+                {
                     bufferNew[x, y] = 1;
                     continue;
                 }
@@ -82,11 +91,16 @@ public class CaveGenerator {
                 int surroundingWalls = GetSurroundingWallCount(x, y);
 
                 //Use some smoothing rules to generate caves
-                if (surroundingWalls > 4) {
+                if (surroundingWalls > 4)
+                {
                     bufferNew[x, y] = 1;
-                } else if (surroundingWalls == 4) {
+                }
+                else if (surroundingWalls == 4)
+                {
                     bufferNew[x, y] = bufferOld[x, y];
-                } else {
+                }
+                else
+                {
                     bufferNew[x, y] = 0;
                 }
             }
@@ -96,20 +110,23 @@ public class CaveGenerator {
         (bufferOld, bufferNew) = (bufferNew, bufferOld);
     }
 
-
-
     //Given a cell, how many of the 8 surrounding cells are walls?
-    private int GetSurroundingWallCount(int cellX, int cellY) {
+    private int GetSurroundingWallCount(int cellX, int cellY)
+    {
         int wallCounter = 0;
-        for (int neighborX = cellX - 1; neighborX <= cellX + 1; neighborX ++) {
-            for (int neighborY = cellY - 1; neighborY <= cellY + 1; neighborY++) {
+        for (int neighborX = cellX - 1; neighborX <= cellX + 1; neighborX++)
+        {
+            for (int neighborY = cellY - 1; neighborY <= cellY + 1; neighborY++)
+            {
                 //We dont need to care about being outside of the grid because we are never looking at the border
-                if (neighborX == cellX && neighborY == cellY) { //This is the cell itself and no neighbor!
+                if (neighborX == cellX && neighborY == cellY)
+                { //This is the cell itself and no neighbor!
                     continue;
                 }
 
                 //This neighbor is a wall
-                if (bufferOld[neighborX, neighborY] == 1) {
+                if (bufferOld[neighborX, neighborY] == 1)
+                {
                     wallCounter += 1;
                 }
             }
--- Assets/Scripts/4-generation/TilemapCaveGenerator.cs
+++ Assets/Scripts/4-generation/TilemapCaveGenerator.cs
@@ -14,7 +14,9 @@ public class TilemapCaveGenerator : MonoBehaviour
 {
     [SerializeField]
     Tilemap tilemap = null;
-    [SerializeField] AllowedTiles allowedTiles = null;
+
+    [SerializeField]
+    AllowedTiles allowedTiles = null;
 
     [Tooltip("The tile that represents a wall (an impassable block)")]
     [SerializeField]
@@ -43,8 +45,11 @@ public class TilemapCaveGenerator : MonoBehaviour
     [SerializeField]
     float pauseTime = 1f;
     public int numSteps = 0;
-    [SerializeField] GameObject player;
+
+    [SerializeField]
+    GameObject player;
     private TilemapGraph graph = null;
+
     //public AllowedTiles allowedTiles = null;
     int maxIterations = 50;
     int iterations = 0;
@@ -67,11 +72,8 @@ public class TilemapCaveGenerator : MonoBehaviour
         GenerateAndDisplayTexture(caveGenerator.GetMap());
         StartCoroutine(SimulateCavePattern());
 
-
-
         //Start the simulation
-
-        }
+    }
 
     bool possValid(Vector3 pos, Tilemap map)
     {
@@ -86,49 +88,90 @@ public class TilemapCaveGenerator : MonoBehaviour
         Vector3Int startNode = map.WorldToCell(pos);
         while (attempts < maxAttempts)
         {
-            Debug.Log("Try Number  = "+attempts);
+            Debug.Log("Try Number  = " + attempts);
             //Debug.Log("attempts: " + attempts);
             bool not_duplicate = false;
-            while(!not_duplicate){
+            while (!not_duplicate)
+            {
                 x = Random.Range(0, gridSize);
                 y = Random.Range(0, gridSize);
-                Vector3 generated_pos = new Vector3(x,y,0);
-                if(!positions.Contains(generated_pos)){
+                Vector3 generated_pos = new Vector3(x, y, 0);
+                if (!positions.Contains(generated_pos))
+                {
                     not_duplicate = true;
-                    Debug.Log(generated_pos +"is new");
+                    Debug.Log(generated_pos + "is new");
                 }
-                else{
+                else
+                {
                     Debug.Log(generated_pos + "duplicate");
                 }
                 positions.Add(generated_pos);
             }
 
-
             Vector3 target = new Vector3(x, y, 0);
-            Vector3Int endNode =map.WorldToCell(target);
+            Vector3Int endNode = map.WorldToCell(target);
             List<Vector3Int> shortestPath = BFS.GetPath(graph, startNode, endNode, 1000);
-           // Debug.Log("startNode: " + startNode+"end node = "+endNode);
-           // Debug.Log("unique count: "+uniqeTiles.Count);
-            if (shortestPath.Count >= 100 )
+            // Debug.Log("startNode: " + startNode+"end node = "+endNode);
+            // Debug.Log("unique count: "+uniqeTiles.Count);
+            if (shortestPath.Count >= 100)
             {
-                Debug.Log(attempts+") path from "+startNode +"to "+endNode+" is in size "+shortestPath.Count+" tiles count "+counter+" set_size "+set.Count);
+                Debug.Log(
+                    attempts
+                        + ") path from "
+                        + startNode
+                        + "to "
+                        + endNode
+                        + " is in size "
+                        + shortestPath.Count
+                        + " tiles count "
+                        + counter
+                        + " set_size "
+                        + set.Count
+                );
                 return true;
             }
-            else if(shortestPath.Count > 0)
+            else if (shortestPath.Count > 0)
             {
                 counter++;
-                for(int i = 0; i< shortestPath.Count; i++){
-                   set.Add(shortestPath[i]);
+                for (int i = 0; i < shortestPath.Count; i++)
+                {
+                    set.Add(shortestPath[i]);
                 }
-                Debug.Log(attempts+") shortest path size from "+startNode+ "to "+endNode+ "is  "+shortestPath.Count+" tiles count " +counter+" set_size "+set.Count);
+                Debug.Log(
+                    attempts
+                        + ") shortest path size from "
+                        + startNode
+                        + "to "
+                        + endNode
+                        + "is  "
+                        + shortestPath.Count
+                        + " tiles count "
+                        + counter
+                        + " set_size "
+                        + set.Count
+                );
             }
-            else if(shortestPath.Count == 0){
-                Debug.Log(attempts+") NoPath from "+startNode+ "to "+endNode+ ",shortestpath count =  "+shortestPath.Count+" tiles count "+counter+" set_size "+set.Count);
+            else if (shortestPath.Count == 0)
+            {
+                Debug.Log(
+                    attempts
+                        + ") NoPath from "
+                        + startNode
+                        + "to "
+                        + endNode
+                        + ",shortestpath count =  "
+                        + shortestPath.Count
+                        + " tiles count "
+                        + counter
+                        + " set_size "
+                        + set.Count
+                );
             }
-            if(counter == 100 || set.Count >= 100){
+            if (counter == 100 || set.Count >= 100)
+            {
                 return true;
             }
-            
+
             attempts++;
         }
 
@@ -154,31 +197,38 @@ public class TilemapCaveGenerator : MonoBehaviour
         Debug.Log("-----------check for position------------");
         Vector3 PlayerPos = player.transform.position;
         bool is_on_grass = false;
-        while(!is_on_grass){
-            TileBase tileOnNewPosition = TileOnPosition(PlayerPos);
-        if (allowedTiles.Contain(tileOnNewPosition)) {
-            is_on_grass = true;
-        }
-        else{
-            PlayerPos.x = Random.Range(0, gridSize);
-            PlayerPos.y = Random.Range(0, gridSize);
-        }
-        player.transform.position = PlayerPos; 
-        }
-        while (!valid && iterations < maxIterations )
+        while (!is_on_grass)
         {
-            if(iterations > 0){
-                Vector3 newpos = new Vector3(Random.Range(0, gridSize), Random.Range(0, gridSize), 0);
+            TileBase tileOnNewPosition = TileOnPosition(PlayerPos);
+            if (allowedTiles.Contain(tileOnNewPosition))
+            {
+                is_on_grass = true;
+            }
+            else
+            {
+                PlayerPos.x = Random.Range(0, gridSize);
+                PlayerPos.y = Random.Range(0, gridSize);
+            }
+            player.transform.position = PlayerPos;
+        }
+        while (!valid && iterations < maxIterations)
+        {
+            if (iterations > 0)
+            {
+                Vector3 newpos = new Vector3(
+                    Random.Range(0, gridSize),
+                    Random.Range(0, gridSize),
+                    0
+                );
                 player.transform.position = newpos;
             }
             iterations++;
             valid = possValid(PlayerPos, tilemap);
-            
         }
-        if(valid){
-            Debug.Log("check done in "+iterations+" iterations");
+        if (valid)
+        {
+            Debug.Log("check done in " + iterations + " iterations");
         }
-
     }
 
     //Generate a black or white texture depending on if the pixel is cave or wall
@@ -195,10 +245,10 @@ public class TilemapCaveGenerator : MonoBehaviour
             }
         }
     }
-    
-    private TileBase TileOnPosition(Vector3 worldPosition) {
+
+    private TileBase TileOnPosition(Vector3 worldPosition)
+    {
         Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
         return tilemap.GetTile(cellPosition);
     }
 }
-
